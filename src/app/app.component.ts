9bc405dd7a8bc3f55331577667d2d0f5f099b8aa
Alcenor Costa de Almeida <alcenor@hotmail.com>
2019-02-18 17:04:35 -0300

    Vídeo-aulas 10.10 a 10.13

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  lancamentos = [
-    {tipo: 'RECEITA', descricao: 'Salário mensal', dataVencimento: '10/06/2018', dataPagamento: '',
-    valor: 6500.00, pessoa: 'Daenerys Targaryen'},
-    {tipo: 'RECEITA', descricao: 'Indenização', dataVencimento: '13/07/2018', dataPagamento: '12/07/2018',
-    valor: 3125.52, pessoa: 'Daenerys Targaryen'},
-    {tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '18/07/2018', dataPagamento: '18/07/2018',
-    valor: 1500.00, pessoa: 'Jon Snow'},
-    {tipo: 'DESPESA', descricao: 'Medicamentos', dataVencimento: '01/08/2018', dataPagamento: '01/08/2018',
-    valor: 522.17, pessoa: 'Cersei Baratheon'},
-    {tipo: 'DESPESA', descricao: 'Compras', dataVencimento: '11/09/2018', dataPagamento: '11/09/2018',
-    valor: 689.72, pessoa: 'Sansa Stark'},
-    {tipo: 'DESPESA', descricao: 'Lanche', dataVencimento: '16/09/2018', dataPagamento: '16/09/2018',
-    valor: 17.52, pessoa: 'Oberyn Martell'},
-    {tipo: 'DESPESA', descricao: 'Viagem', dataVencimento: '21/10/2018', dataPagamento: '21/10/2018',
-    valor: 2500.00, pessoa: 'Jaime Lannister'},
-    {tipo: 'DESPESA', descricao: 'Uber', dataVencimento: '24/11/2018', dataPagamento: '',
-    valor: 17.78, pessoa: 'Jon Snow'}
-  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import {InputTextModule} from 'primeng/components/inputtext/inputtext';
 import {ButtonModule} from 'primeng/components/button/button';
 import {TableModule} from 'primeng/components/table/table';
 import {TooltipModule} from 'primeng/components/tooltip/tooltip';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
