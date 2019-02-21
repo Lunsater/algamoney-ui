673576620ae68ef38777844f63e5b578a3ec6473
Alcenor Costa de Almeida <alcenor@hotmail.com>
2019-02-20 22:52:10 -0300

    Vídeo-aulas 11.1 a 11.8

--- src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
+++ src/app/lancamentos-pesquisa/lancamentos-pesquisa.component.ts
@@ -8,22 +8,22 @@ import { Component, OnInit } from '@angular/core';
 export class LancamentosPesquisaComponent implements OnInit {
 
   lancamentos = [
-    {tipo: 'RECEITA', descricao: 'Salário mensal', dataVencimento: '10/06/2018', dataPagamento: '',
-    valor: 6500.00, pessoa: 'Daenerys Targaryen'},
-    {tipo: 'RECEITA', descricao: 'Indenização', dataVencimento: '13/07/2018', dataPagamento: '12/07/2018',
-    valor: 3125.52, pessoa: 'Daenerys Targaryen'},
-    {tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: '18/07/2018', dataPagamento: '18/07/2018',
-    valor: 1500.00, pessoa: 'Jon Snow'},
-    {tipo: 'DESPESA', descricao: 'Medicamentos', dataVencimento: '01/08/2018', dataPagamento: '01/08/2018',
-    valor: 522.17, pessoa: 'Cersei Baratheon'},
-    {tipo: 'DESPESA', descricao: 'Compras', dataVencimento: '11/09/2018', dataPagamento: '11/09/2018',
-    valor: 689.72, pessoa: 'Sansa Stark'},
-    {tipo: 'DESPESA', descricao: 'Lanche', dataVencimento: '16/09/2018', dataPagamento: '16/09/2018',
-    valor: 17.52, pessoa: 'Oberyn Martell'},
-    {tipo: 'DESPESA', descricao: 'Viagem', dataVencimento: '21/10/2018', dataPagamento: '21/10/2018',
-    valor: 2500.00, pessoa: 'Jaime Lannister'},
-    {tipo: 'DESPESA', descricao: 'Uber', dataVencimento: '24/11/2018', dataPagamento: '',
-    valor: 17.78, pessoa: 'Jon Snow'}
+    {tipo: 'RECEITA', descricao: 'Salário mensal', dataVencimento: new Date(2018, 6, 10),
+      dataPagamento: null, valor: 6500.00, pessoa: 'Daenerys Targaryen'},
+    {tipo: 'RECEITA', descricao: 'Indenização', dataVencimento: new Date(2018, 7, 13),
+      dataPagamento: new Date(2018, 7, 12), valor: 3125.52, pessoa: 'Daenerys Targaryen'},
+    {tipo: 'DESPESA', descricao: 'Aluguel', dataVencimento: new Date(2018, 7, 18),
+      dataPagamento: new Date(2018, 7, 18), valor: 1500.00, pessoa: 'Jon Snow'},
+    {tipo: 'DESPESA', descricao: 'Medicamentos', dataVencimento: new Date(2018, 8, 1),
+      dataPagamento: new Date(2018, 8, 1), valor: 522.17, pessoa: 'Cersei Baratheon'},
+    {tipo: 'DESPESA', descricao: 'Compras', dataVencimento: new Date(2018, 9, 11),
+      dataPagamento: new Date(2018, 9, 11), valor: 689.72, pessoa: 'Sansa Stark'},
+    {tipo: 'DESPESA', descricao: 'Lanche', dataVencimento: new Date(2018, 9, 16),
+      dataPagamento: new Date(2018, 9, 16), valor: 17.52, pessoa: 'Oberyn Martell'},
+    {tipo: 'DESPESA', descricao: 'Viagem', dataVencimento: new Date(2018, 10, 21),
+      dataPagamento: new Date(2018, 10, 21), valor: 2500.00, pessoa: 'Jaime Lannister'},
+    {tipo: 'DESPESA', descricao: 'Uber', dataVencimento: new Date(2018, 11, 24),
+      dataPagamento: null, valor: 17.78, pessoa: 'Jon Snow'}
   ];
 
   constructor() { }
